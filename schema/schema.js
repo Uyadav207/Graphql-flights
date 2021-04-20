ac6d3f5ad6089e62a21a283af336cd5a8a07901a
Uyadav207 <yadavutkarsh207@gmail.com>
2021-04-20 19:28:17 +0530

    update: Query for getting Passenger info from flight and passenger id

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,9 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { 
-    GraphQLObjectType, 
-    GraphQLString, 
+const {
+    GraphQLObjectType,
+    GraphQLString,
     GraphQLInt,
     GraphQLID,
     GraphQLBoolean,
@@ -14,42 +14,49 @@ const {
 // dummy test data
 
 const Flights = [
-    { name: "AirIndia" , from: "lucknow" , to: "delhi" , fair: 2504 , class: "Business" , id: "1" },
-    { name: "AirAsia" , from: "delhi" , to: "mumbai" , fair: 9873 , class: "economy" , id: "2" },
-    { name: "Indigo" , from: "odissa" , to: "banglore" , fair: 5472 , class: "Business" , id: "3" },
-    { name: "boing" , from: "USA" , to: "chicago" , fair: 6764 , class: "A" , id: "4" },
-    { name: "emirates" , from: "Dubai" , to: "NYC" , fair: 9533 , class: "VIP" , id: "5" },
+    { name: "AirIndia", from: "lucknow", to: "delhi", fair: 2504, class: "Business", id: "1",},
+    { name: "AirAsia", from: "delhi", to: "mumbai", fair: 9873, class: "economy", id: "2" },
+    { name: "Indigo", from: "odissa", to: "banglore", fair: 5472, class: "Business", id: "3",},
+    { name: "boing", from: "USA", to: "chicago", fair: 6764, class: "A", id: "4" },
+    { name: "emirates", from: "Dubai", to: "NYC", fair: 9533, class: "VIP", id: "5" },
 ];
 
 const Passengers = [
-    { name: "utkarsh", age: 20, luggage: { carry: true, number: 2, type: "Bag", weight: 10 },  id: "1", flightId: "2" },
-    { name: "Priyanka Yadav", age: 26, luggage: { carry: true, number: 1, type: "Suitcase", weight: 20 },  id: "1", flightId: "1" },
-    { name: "Ekansh", age: 20, luggage: { carry: false, number: 0, type: "null", weight: 0 },  id: "2" , flightId: "2" },
-    { name: "vijay Laxmi Yadav", age: 50, luggage: { carry: false, number: 0, type: "null", weight: 0 },  id: "3" , flightId: "1" },
-    { name: "Ashok Kumar Yadav", age: 52, luggage: { carry: true, number: 3, type: "Briefcase", weight: 10 },  id: "4" , flightId: "3" },
-    { name: "Ashok Kumar Yadav", age: 52, luggage: { carry: true, number: 3, type: "Briefcase", weight: 10 },  id: "5" , flightId: "2" }
+    { name: "utkarsh", age: 20, luggage: { carry: true, number: 2, type: "Bag", weight: 10 }, id: "1", flightId: "2" },
+    { name: "Priyanka Yadav", age: 26, luggage: { carry: true, number: 1, type: "Suitcase", weight: 20 }, id: "2", flightId: "1" },
+    { name: "Ekansh", age: 20, luggage: { carry: false, number: 0, type: "null", weight: 0 }, id: "3", flightId: "2" },
+    { name: "vijay Laxmi Yadav", age: 50, luggage: { carry: false, number: 0, type: "null", weight: 0 }, id: "4", flightId: "1" },
+    { name: "Ashok Kumar Yadav", age: 52, luggage: { carry: true, number: 3, type: "Briefcase", weight: 10 }, id: "5", flightId: "3" },
+    { name: "Ashok Kumar Yadav", age: 52, luggage: { carry: true, number: 3, type: "Briefcase", weight: 10 }, id: "6", flightId: "2" }
 ]
 
+
+const LuggageType = new GraphQLObjectType({
+    name: 'Luggage',
+    fields: () => ({
+        carry: { type: GraphQLBoolean },
+        number: { type: GraphQLInt },
+        type: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+    })
+});
 
 const FlightType = new GraphQLObjectType({
     name: 'Flight',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
+        age: { type: GraphQLInt },
         from: { type: GraphQLString },
         to: { type: GraphQLString },
         fair: { type: GraphQLInt },
-        class: { type: GraphQLString }
-    })
-});
-
-const LuggageType = new GraphQLObjectType({
-    name: 'Luggage',
-    fields: () => ({
-    carry: { type: GraphQLBoolean }, 
-    number: { type: GraphQLInt }, 
-    type: { type: GraphQLString }, 
-    weight: { type: GraphQLInt },
+        class: { type: GraphQLString },
+        passengers: {
+            type: new GraphQLList(PassengerType),
+            resolve(parent, args) {
+                return _.filter(Passengers, { flightId: parent.id })
+            }
+        }
     })
 });
 
@@ -57,13 +64,12 @@ const PassengerType = new GraphQLObjectType({
     name: 'Passenger',
     fields: () => ({
         id: { type: GraphQLID },
-        name: {type: GraphQLString },
+        name: { type: GraphQLString },
         age: { type: GraphQLInt },
-        luggage:  { type: new GraphQLNonNull(LuggageType) },
+        luggage: { type: new GraphQLNonNull(LuggageType) },
         flight: {
             type: FlightType,
-            resolve(parent, args){
-                console.log(parent);
+            resolve(parent, args) {
                 return _.find(Flights, { id: parent.flightId })
             }
         }
@@ -71,20 +77,19 @@ const PassengerType = new GraphQLObjectType({
 });
 
 const RootQuery = new GraphQLObjectType({
-    name:  'RootQueryType',
+    name: 'RootQueryType',
     fields: {
         flight: {
             type: FlightType,
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                // code to get flight from database.
-                return _.find(Flights, { id:args.id });
+            resolve(parent, args) {
+                return _.find(Flights, { id: args.id });
             }
         },
         passenger: {
             type: PassengerType,
             args: { id: { type: GraphQLID } },
-            resolve(parents, args){
+            resolve(parents, args) {
                 return _.find(Passengers, { id: args.id });
             }
         }
